078e84a0f2c5f30c31071f719619488bc5eb5b70
uriloop <orlopez@elpuig.xeill.net>
2021-12-19 22:02:06 +0100

    1.4

--- app/src/main/java/com/example/myapplication2000/MainActivity.java
+++ app/src/main/java/com/example/myapplication2000/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication2000;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.myapplication2000.databinding.ActivityMainBinding;
@@ -15,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView((binding = ActivityMainBinding.inflate(getLayoutInflater())).getRoot());
 
-
-
         setSupportActionBar(binding.toolbar);
+
     }
 }
